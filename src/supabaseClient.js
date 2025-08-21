b25828bc32b687d8f372eb27f2d5cadf7721c4cc
GeorgiM13 <gmilanov9@gmail.com>
2025-08-21 13:48:42 +0300

    ...

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://vjwxfbvmznvrfcaekqqo.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqd3hmYnZtem52cmZjYWVrcXFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUxNjA0ODYsImV4cCI6MjA3MDczNjQ4Nn0.pKhoB68TGuXe6PiXifpxkrSU-ZVFro9Qy7cEvNQ1KpM';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
